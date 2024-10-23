7463b322fe8b54cfc8837350bbd36603e61976d2
Nick Grato <ngrato@gmail.com>
2024-10-23 09:19:23 -0700

    adding new vite for storybook build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  // Set the base path for GitHub Pages deployment
+  base: '/react-element-forge/', // Update this with your repository name
+
   plugins: [
     react(),
     dts({
@@ -12,13 +15,16 @@ export default defineConfig({
       outDir: 'dist/types',
     }),
   ],
+
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "src/styles/global.scss";`, // Optional: for global SCSS variables/mixins
+        // Optional: for global SCSS variables/mixins
+        additionalData: `@use "src/styles/global.scss";`,
       },
     },
   },
+
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
@@ -33,8 +39,24 @@ export default defineConfig({
           react: 'React',
           'react-dom': 'ReactDOM',
         },
+        // Configure code-splitting and dynamic import handling
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
       },
     },
     minify: 'esbuild', // Ensure minification is enabled
   },
-});
+
+  // Optional: Add server configuration if needed for local development
+  server: {
+    port: 3000, // Set the port if you need one
+    open: true, // Automatically open the browser on server start
+  },
+})
